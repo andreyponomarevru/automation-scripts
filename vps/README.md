8d5811a5f88154008c1da68fbc61ad94e73f7817
Andrey Ponomarev <info@andreyponomarev.ru>
2022-04-16 17:00:58 +0300

    Update scripts

--- vps/README.md
+++
@@ -1,1 +0,0 @@
-VPS setup scripts
--- vps/simplecloud.ru/rebuild-os.sh
+++ vps/simplecloud.ru/rebuild-os.sh
@@ -2,10 +2,11 @@
 
 set -eu
 
-# API token can be ibtained at https://simplecloud.ru/cpanel/#/profile
+# API token can be obtained at https://simplecloud.ru/cpanel/#/profile
 # It is invalidated after 30 minutes.
 API_TOKEN="$1"
-VPS_ID="271099"
+# `VPS_ID` is displayed to the right of your VPS name at https://simplecloud.ru/cpanel
+VPS_ID="$2"
 
 
 # `373` is ID of Ubuntu 20.04 image
