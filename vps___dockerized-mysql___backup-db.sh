118b7013076155b238b60474026737a7587c0078
Andrey Ponomarev <info@andreyponomarev.ru>
2022-09-18 22:42:32 +0300

    Update

--- vps___dockerized-mysql___backup-db.sh
+++
@@ -1,5 +0,0 @@
-# Backup
-docker exec CONTAINER /usr/bin/mysqldump -u root --password=root DATABASE > backup.sql
-
-# Restore
-cat backup.sql | docker exec -i CONTAINER /usr/bin/mysql -u root --password=root DATABASE
