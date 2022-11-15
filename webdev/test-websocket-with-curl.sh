8152876353f45de93fde092b2afc069ad7bfe0a8
Andrey Ponomarev <info@andreyponomarev.ru>
2022-11-15 13:02:21 +0300

    Update sec-websocket-key and url

--- webdev/test-websocket-with-curl.sh
+++ webdev/test-websocket-with-curl.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# If you get "Missing or invalid Sec-WebSocket-Key header" error, just try to pass another sec-websocket-key
+
 curl \
   --include \
   --no-buffer \
   --header "connection: upgrade" \
   --header "upgrade: websocket" \
-  --header "host: localhost:5000" \
-  --header "origin: http://localhost:5000" \
-  --header "sec-websocket-key: SGVsbG8sIHdvcmxkIQ==" \
+  --header "host: localhost" \
+  --header "origin: http://localhost:8080" \
+  --header "sec-websocket-key: dGhlIHNhbXBsZSBub25jZQ==" \
   --header "sec-websocket-version: 13" \
-  http://localhos
+  http://localhost:8080
 
 echo
