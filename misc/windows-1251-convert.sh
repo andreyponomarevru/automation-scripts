29028d3ca375bfbb35cbfeccaa76660e79bd8cf3
Andrey Ponomarev <info@andreyponomarev.ru>
2023-07-02 20:29:26 +0300

    Add description

--- misc/windows-1251-convert.sh
+++ misc/windows-1251-convert.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# DESCRIPTION
+#   Convert Cyrillic text files using Windows-1251 encoding to Linux UTF-8 
+#
+# SYNTAX 
+#   ./script Readme1.txt > Readme2.txt
+
 iconv -f windows-1251 -t UTF-8 $1 > $2 
