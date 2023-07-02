d9d31c784cebb8b03ab901615bb307e246379f90
Andrey Ponomarev <info@andreyponomarev.ru>
2023-07-02 13:38:47 +0300

    Add minor changes

--- misc/m3u8-video-downloader.sh
+++ misc/m3u8-video-downloader.sh
@@ -6,18 +6,21 @@
 #
 # SYNTAX 
 #       <input file name> <output file name>
-#     - example: ./script "https://website.com/video.m3u8" "Output file name.mp4"
+#     - example: ./script "https://website.com/video.m3u8[optional params]" "Output file name.mp4"
+
+set -euo pipefail
 
 INPUT_FILE="$1"
 OUTPUT_FILE="$2"
 
 if \
   ffmpeg \
-    -protocol_whitelist file,http,https,tcp,tls,crypto \
     -i "$INPUT_FILE" \
-    -c copy "$OUTPUT_FILE"; \
+    -c copy \
+    -bsf:a aac_adtstoasc \
+    "$OUTPUT_FILE"; \
 then \
-  echo "Done. Video successfully saved to '$OUTPUT_FILE'"
+  echo -e "\nDone. Video successfully saved to '$OUTPUT_FILE'"
 else 
   echo "Error. Something went wrong"
 fi
