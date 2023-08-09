4d3478c3c65176f10fb5a6cdf1f0f8f9f06107d1
Andrey Ponomarev <info@andreyponomarev.ru>
2023-08-09 15:09:15 +0300

    Make a bit more POSIX-compatible

--- audio/dts-to-multiple-wav.sh
+++ audio/dts-to-multiple-wav.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-set -euo pipefail
+-set -euo pipefail
 
-# This script works only wuth DTS 5.1
+# This script works only with DTS 5.1
 
 DTS_FILE_NAME="$1"
 DTS_VERSION="$2"
 
-if [ "$DTS_VERSION" == "5.1" ]; then
+if [ "$DTS_VERSION" = "5.1" ]
+then
   ffmpeg \
     -i "$DTS_FILE_NAME" \
     -filter_complex "channelsplit=channel_layout=5.1[FL][FR][FC][LFE][BL][BR]" \
@@ -19,7 +20,8 @@ if [ "$DTS_VERSION" == "5.1" ]; then
     -map "[BR]" "${DTS_FILE_NAME}_back_right.wav"
 fi
 
-if [ "$DTS_VERSION" == "7.1" ]; then
+if [ "$DTS_VERSION" = "7.1" ]
+then
   ffmpeg \
     -i "$DTS_FILE_NAME" \
     -filter_complex "channelsplit=channel_layout=7.1[FL][FR][FC][LFE][BL][BR][FLC][FRC]" \
@@ -32,5 +34,5 @@ if [ "$DTS_VERSION" == "7.1" ]; then
     -map "[FLC]" "${DTS_FILE_NAME}_front_left_center.wav" \
     -map "[FRC]" "${DTS_FILE_NAME}_front_right_center.wav"
 else
-  echo "This script supports only DTS version 5.1 and 7.1"
+  printf "This script supports only DTS version 5.1 and 7.1"
 fi
